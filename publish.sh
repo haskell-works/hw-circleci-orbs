490739b6071126902df92011eeeb19e19aa1b86e
John Ky <john.ky@iohk.io>
2020-08-21 16:47:13 +1000

    New publish.sh script

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+orb="$1"
+tag="$2"
+
+circleci config pack "$orb" > "/tmp/$orb.yml"
+circleci config validate "/tmp/$orb.yml"
+circleci orb publish "/tmp/$orb.yml" "haskell-works/$orb@$tag"
